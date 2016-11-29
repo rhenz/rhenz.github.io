24e539ccbeba93e48721df544f62138ab356f615
Rhenz <salvador.rhenz@gmail.com>
2016-11-30 01:27:04 +0800

    update index.html

--- app/main.js
+++ app/main.js
@@ -11,14 +11,14 @@ app.controller('projectController', function ($scope) {
   }
 });
 
-app.controller('rhenzController', function ($scope) {
+app.controller('profileController', function ($scope) {
   $scope.jls = {
     username: 'rhenzsalvador',
     name: 'John Lawrence Salvador',
     email: 'salvador.rhenz@gmail.com',
     phone: '+639152688041',
     fbLink: 'https://www.facebook.com/jlcsalvador',
-    linkedInLink: 'https://www.linkedin.com/in/ceosilvajr',
+    linkedInLink: 'https://www.linkedin.com/in/',
     twitterLink: 'https://twitter.com/rhenzsalvador',
     stackLink: 'http://stackoverflow.com/users/4518934/rhenz'
   };
